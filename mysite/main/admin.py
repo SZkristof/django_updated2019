f03e0d52b98e2968cca1017072c860a7b9ce2e51
Unknown <szabokristof0@gmail.com>
2019-01-29 23:31:41 +0100

    Changing admin fieldsets

--- mysite/main/admin.py
+++ mysite/main/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from .models import Tutorial
 # Register your models here.
+
+class TutorialAdmin(admin.ModelAdmin):
+# This makes seperated fields.
+    fieldsets = [
+        ("Title/date", {"fields": ["tutorial_title", "tutorial_published"]}),
+        ("Content", {"fields": ["tutorial_content"]})
+    ]
+
+admin.site.register(Tutorial, TutorialAdmin)
